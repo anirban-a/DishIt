74f2f1f168a9fcb129c8ff163363bf0e2f07d672
Anirban Acharya <acharya.anirban9@gmail.com>
2024-03-11 22:55:49 -0400

    initial setup fix

---
+++ src/main/java/com/sageapp/dishit/DishitApplication.java
@@ -0,0 +1,13 @@
+package com.sageapp.dishit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DishitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sageapp/dishit/DishitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sageapp.dishit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DishitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
